109208770ad500b964854ca63183c370c30008e0
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-03-06 20:46:50 +0100

    Kundvagn och local storage

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let kundvagn = [];
+let antalProdukter = 0;
+const Produkter = document.querySelector(".dropdown ul");
+const totalPrisHTML = document.querySelector(".dropdown >p");
+let totalPris = 0;
 
 function LäggTillIKundvagn(namn, pris) {
   const produkt = {
@@ -6,5 +10,25 @@ function LäggTillIKundvagn(namn, pris) {
     pris: pris,
   };
   kundvagn.push(produkt);
-  alert(JSON.stringify(kundvagn));
+  /* alert(JSON.stringify(kundvagn)); */
+
+  const Namn = document.createElement("p");
+  Namn.innerHTML = namn;
+
+  const Pris = document.createElement("p");
+  Pris.innerHTML = pris + " kr";
+
+  const productHtml = document.createElement("li");
+  productHtml.appendChild(Namn);
+  productHtml.appendChild(Pris);
+  Produkter.appendChild(productHtml);
+
+  totalPris += pris;
+  totalPrisHTML.innerHTML = "Summa: " + totalPris + " kr";
+
+  localStorage.setItem("kundvagn", JSON.stringify(kundvagn));
+  localStorage.setItem("Totala-Priset", totalPris);
 }
+
+kundvagn = JSON.parse(localStorage.getItem("kundvagn"));
+totalPris = parseInt(localStorage.getItem("Totala-Priset"));
