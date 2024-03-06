2b880dc008b322b2b16fe6293218f3d60a293223
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-03-06 21:05:22 +0100

    Pillat lite med local storage

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let kundvagn = [];
-let antalProdukter = 0;
+
 const Produkter = document.querySelector(".dropdown ul");
 const totalPrisHTML = document.querySelector(".dropdown >p");
 let totalPris = 0;
+
+function uppdateraKundvagn() {
+  for (let index = 0; index < kundvagn.length; index++) {
+    const Namn = document.createElement("p");
+    Namn.innerHTML = kundvagn[index].namn;
+
+    const Pris = document.createElement("p");
+    Pris.innerHTML = kundvagn[index].pris + " kr";
+
+    const productHtml = document.createElement("li");
+    productHtml.appendChild(Namn);
+    productHtml.appendChild(Pris);
+    Produkter.appendChild(productHtml);
+
+    totalPrisHTML.innerHTML = "Summa: " + totalPris + " kr";
+  }
+}
 
 function LäggTillIKundvagn(namn, pris) {
   const produkt = {
@@ -12,23 +29,19 @@ function LäggTillIKundvagn(namn, pris) {
   kundvagn.push(produkt);
   /* alert(JSON.stringify(kundvagn)); */
 
-  const Namn = document.createElement("p");
-  Namn.innerHTML = namn;
-
-  const Pris = document.createElement("p");
-  Pris.innerHTML = pris + " kr";
-
-  const productHtml = document.createElement("li");
-  productHtml.appendChild(Namn);
-  productHtml.appendChild(Pris);
-  Produkter.appendChild(productHtml);
-
   totalPris += pris;
-  totalPrisHTML.innerHTML = "Summa: " + totalPris + " kr";
 
   localStorage.setItem("kundvagn", JSON.stringify(kundvagn));
   localStorage.setItem("Totala-Priset", totalPris);
+  uppdateraKundvagn();
 }
 
+function rensaKundvagn() {
+  totalPris = 0;
+  kundvagn = [];
+  localStorage.clear();
+  uppdateraKundvagn();
+}
 kundvagn = JSON.parse(localStorage.getItem("kundvagn"));
 totalPris = parseInt(localStorage.getItem("Totala-Priset"));
+uppdateraKundvagn();
